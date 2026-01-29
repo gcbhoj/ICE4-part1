44b18f2b535d5b680f9388cfa393b8747094e771
BhojGC <gcbhoj@gmail.com>
2026-01-29 09:57:40 -0500

    inital commit added appointment class with id, date and time

---
+++ src/main/java/com/example/demo/beans/appointment.java
@@ -0,0 +1,12 @@
+package com.example.demo.beans;
+
+import java.time.LocalDateTime;
+
+public class appointment {
+	
+	private Long id;
+	private LocalDateTime appointmentTime;
+	private LocalDateTime appointmentDate;
+	private String reason;
+
+}
