372efe9c3279236223cabfa50573e67ef4305d2f
BhojGC <gcbhoj@gmail.com>
2026-01-29 10:02:24 -0500

    added lomok annotation

--- src/main/java/com/example/demo/beans/appointment.java
+++ src/main/java/com/example/demo/beans/appointment.java
@@ -2,6 +2,13 @@ package com.example.demo.beans;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+
 public class appointment {
 	
 	private Long id;
