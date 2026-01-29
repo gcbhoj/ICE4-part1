32b0b6593ba9462ea56a625bfa4a56f72e4df807
BhojGC <gcbhoj@gmail.com>
2026-01-29 09:54:53 -0500

    initialized git local

---
+++ src/main/java/com/example/demo/Ice4Part1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ice4Part1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ice4Part1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/Ice4Part1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ice4Part1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
